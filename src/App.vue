95bd2df2b24c641d8a756372dbccdd265a94f7ab
salvatorevarisano <salvator3varisano@gmail.com>
2021-05-10 19:44:29 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 <script>
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
-
+import axios from 'axios'
 
 export default {
   name: 'App',
@@ -20,6 +20,7 @@ export default {
   data() {
     return {
       SearchMovie: '',
+      apiMoviesURL: 'https://api.themoviedb.org/3/search/movie?api_key=990553a5085849ae45ce07713d45c579&query=',
     }
   },
 
@@ -27,6 +28,16 @@ export default {
     searchMovies(userSearch) {
      // console.log('ricerca film', userSearch);
       this.SearchMovie = userSearch;
+      if(this.SearchMovie !== '') {
+                 return axios.get(this.apiMoviesURL + this.SearchMovie)
+                    .then((res) => {
+                        // this.test = res.data;
+                        this.SearchMovie = res.data;
+                        console.log(res.data);
+                    }).catch((error) => {
+                        console.log(error);
+                    })
+            }
       //console.log(this.search);
     }
   }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,29 +6,15 @@
 </template>
 
 <script>
-import axios from 'axios';
 export default {
     name: 'Main',
     props: ['SearchMovie'],
     data() {
         return {
-            apiMoviesURL: `https://api.themoviedb.org/3/search/movie?api_key=990553a5085849ae45ce07713d45c579&query=${this.SearchMovie}`,
+            
+
         }
     },
-    computed() {
-        filterMovies() {
-            if(this.SearchMovie !== '') {
-                axios.get(this.apiMoviesURL)
-                    .then((res) => {
-                        console.log(res.data);
-                    }).catch((error) => {
-                        console.log(error);
-                    })
-            }
-        }
-            
-        
-    }
 
 }
 </script>
