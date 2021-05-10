1e1629ec8c4a7b53dff0c04654e7a42132ab4b56
salvatorevarisano <salvator3varisano@gmail.com>
2021-05-10 16:01:18 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header @searchKeyword="searchMovies" />
+    <Main :SearchMovie="SearchMovie"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Main,
+  },
+
+  data() {
+    return {
+      SearchMovie: '',
+    }
+  },
+
+  methods: {
+    searchMovies(userSearch) {
+     // console.log('ricerca film', userSearch);
+      this.SearchMovie = userSearch;
+      //console.log(this.search);
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+      main content
+      {{SearchMovie}}
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    props: ['SearchMovie'],
+
+}
+</script>
+
+<style>
+
+</style>
