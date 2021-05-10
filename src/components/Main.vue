7f9f055f19492017c1a9d4febbe69eab425d58e6
salvatorevarisano <salvator3varisano@gmail.com>
2021-05-10 23:40:54 +0200

    add flags

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,8 +3,12 @@
       <ul v-for="(movie, i) in searcheredMovie" :key="i">
           <li>{{movie.title}}</li>
           <li>{{movie.original_title}}</li>
-          <li>{{movie.original_language}}</li>
+          <!-- <li>{{printFlag(movie.original_language)}}</li>  -->
+          <li v-if="hasFlag(movie.original_language)"><img class="flag" :src="require(`../assets/img/${movie.original_language}.png`)"></li>
+          <li v-else >{{movie.original_language}}</li>
           <li>{{movie.vote_average}}</li>
+
+          
       </ul>
   </main>
 </template>
@@ -15,14 +19,32 @@ export default {
     props: ['searcheredMovie'],
     data() {
         return {
-            
+            flags: ['it', 'en'],
+            flag: false,
 
         }
     },
+    methods: {
+        // img non viene interpretato come tag ma come stringa
+        //  printFlag(language) {
+        //      if(this.flags.includes(language)) {
+        //          return `<img :src="require(../assets/img/${language}.png)" alt="${language}">`;
+        //      }
+        //      return language
+        //  }
 
+        hasFlag(language) {
+            if(this.flags.includes(language)){
+                return this.flag = true;
+            }
+        },
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    .flag {
+        width: 20px;
+    }
 
 </style>
