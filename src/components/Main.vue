8d4f90223b96cf2dfcc16455a9f9ae59b96cca05
salvatorevarisano <salvator3varisano@gmail.com>
2021-05-10 16:29:11 +0200

    add axios

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,9 +6,23 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'Main',
     props: ['SearchMovie'],
+    data() {
+        return {
+            apiMoviesURL: 'https://api.themoviedb.org/3/search/movie?api_key=990553a5085849ae45ce07713d45c579',
+        }
+    },
+    created() {
+        axios.get(this.apiMoviesURL)
+            .then((res) => {
+                console.log(res.data);
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
 
 }
 </script>
