c0e6ee2e682f3aa30f543c4427151b78399c6d93
salvatorevarisano <salvator3varisano@gmail.com>
2021-05-12 00:07:26 +0200

    Milestone 3

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,21 @@
 <template>
   <main>
       <ul v-for="(movie, i) in searcheredMovie" :key="i">
-          <li> <img :src="imagePathURLPartOne + movie.poster_path" :alt="movie.title || movie.name" class="poster"></li>
+          <li v-if="movie.poster_path !== undefined && movie.poster_path !== null" > <img :src="imagePathURLPartOne + movie.poster_path" :alt="movie.title || movie.name" class="poster"></li>
+          <li v-else ><div><img class="poster" src="@/assets/img/censored.png" alt=""></div></li>
           <li>{{movie.title || movie.name}}</li>
           <li>{{movie.original_title || movie.original_name}}</li>
-          <!-- <li>{{printFlag(movie.original_language)}}</li>  -->
           <li v-if="hasFlag(movie.original_language)"><img class="flag" :src="require(`../assets/img/${movie.original_language}.png`)" :alt="movie.original_language"></li>
           <li v-else >{{movie.original_language}}</li>
-          <li>{{movie.vote_average}}</li>
+          <li v-if="(movie.vote_average > 0)">
+              <i class="fas fa-star" v-for="(star, i) in stars(movie.vote_average)" :key="'s' + i"></i>
+              <i class="far fa-star" v-for="(emptyStar, i) in ( 5 - stars(movie.vote_average) )" :key="'es' + i"></i>
+          </li>
+          <li v-else >
+              <i class="far fa-star" v-for="(emptyStar, i) in  5" :key="'fes' + i"></i>
+          </li>
 
-          
+
       </ul>
   </main>
 </template>
@@ -21,23 +27,22 @@ export default {
     data() {
         return {
             flags: ['it', 'en'],
-            imagePathURLPartOne: 'https://image.tmdb.org/t/p/w500/'
+            imagePathURLPartOne: 'https://image.tmdb.org/t/p/w342/',
         }
     },
     methods: {
-        // img non viene interpretato come tag ma come stringa
-        //  printFlag(language) {
-        //      if(this.flags.includes(language)) {
-        //          return `<img :src="require(../assets/img/${language}.png)" alt="${language}">`;
-        //      }
-        //      return language
-        //  }
 
         hasFlag(language) {
             if(this.flags.includes(language)){
                 return true;
             }
         },
+
+        stars(vote) {
+            const starsNumber = vote / 2;
+            return  Math.round(starsNumber);
+
+        }
     }
 }
 </script>
@@ -47,6 +52,8 @@ export default {
         width: 20px;
     }
     .poster {
-        height: 300px;
+
+        width: 300px;
     }
+
 </style>
