d2021c33a8748d35d64b9ddd5318c0c18ad036af
salvatorevarisano <salvator3varisano@gmail.com>
2021-05-12 18:30:23 +0200

    fix

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,22 +1,40 @@
 <template>
   <main>
-      <ul v-for="(movie, i) in searcheredMovie" :key="i">
-          <li v-if="movie.poster_path !== undefined && movie.poster_path !== null" > <img :src="imagePathURLPartOne + movie.poster_path" :alt="movie.title || movie.name" class="poster"></li>
-          <li v-else ><div><img class="poster" src="@/assets/img/censored.png" alt=""></div></li>
-          <li>{{movie.title || movie.name}}</li>
-          <li>{{movie.original_title || movie.original_name}}</li>
-          <li v-if="hasFlag(movie.original_language)"><img class="flag" :src="require(`../assets/img/${movie.original_language}.png`)" :alt="movie.original_language"></li>
-          <li v-else >{{movie.original_language}}</li>
-          <li v-if="(movie.vote_average > 0)">
-              <i class="fas fa-star" v-for="(star, i) in stars(movie.vote_average)" :key="'s' + i"></i>
-              <i class="far fa-star" v-for="(emptyStar, i) in ( 5 - stars(movie.vote_average) )" :key="'es' + i"></i>
-          </li>
-          <li v-else >
-              <i class="far fa-star" v-for="(emptyStar, i) in  5" :key="'fes' + i"></i>
-          </li>
+      <section>
+          <!-- <Card  v-for="movie in searcheredMovie" :key="movie.id" /> -->
 
+          <div class="card">
 
-      </ul>
+                <!-- movie poster -->
+                <img class="poster" v-if="movie.poster_path !== undefined && movie.poster_path !== null" 
+                    :src="imagePathURLPartOne + movie.poster_path"
+                    :alt="movie.title || movie.name" 
+                >
+                <img v-else class="poster" src="@/assets/img/censored.png" :alt="movie.title || movie.name">
+
+                <!-- movie info -->
+                <div class="overlay">
+                    <h2>{{movie.title || movie.name}}</h2>
+                    <div v-show="(movie.title || movie.name) !== (movie.original_title || movie.original_name)">
+                        <span>original title</span>
+                        <h3>{{movie.original_title || movie.original_name}}</h3>
+                    </div>
+                    <div v-if="hasFlag(movie.original_language)">
+                        <img class="flag" :src="require(`../assets/img/${movie.original_language}.png`)" :alt="movie.original_language">
+                    </div>
+                    <div v-else >
+                        {{movie.original_language}}
+                    </div>
+                    <span v-if="(movie.vote_average > 0)">
+                        <i class="fas fa-star" v-for="(star, i) in stars(movie.vote_average)" :key="'s' + i"></i>
+                        <i class="far fa-star" v-for="(emptyStar, i) in ( 5 - stars(movie.vote_average) )" :key="'es' + i"></i>
+                    </span>
+                    <span v-else >
+                        <i class="far fa-star" v-for="(emptyStar, i) in  5" :key="'fes' + i"></i>
+                    </span>
+                </div>
+          </div>
+      </section>
   </main>
 </template>
 
